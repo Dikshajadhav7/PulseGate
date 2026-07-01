5caed873e670ed763ae386dcf1633d4f9d18c2be
25182_DIKSHA JADHAV <Dikshajadhav7@users.noreply.github.com>
2026-07-01 15:33:40 +0530

    feat(eureka-server): setup Spring Cloud Eureka Server

---
+++ backend/eureka-server/src/test/java/com/pulsegate/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsegate.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
